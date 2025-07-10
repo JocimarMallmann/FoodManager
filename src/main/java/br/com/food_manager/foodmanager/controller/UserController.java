c59d4f1a6c4fff647eac4dab2cb0497196e91a8d
Lemao <jocimarmallmann@hotmail.com>
2025-07-09 23:09:08 -0300

    feat: config ambientes dev e prod

--- src/main/java/br/com/food_manager/foodmanager/controller/UserController.java
+++ src/main/java/br/com/food_manager/foodmanager/controller/UserController.java
@@ -1,4 +1,23 @@
 package br.com.food_manager.foodmanager.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping
+    public ResponseEntity<String> getTest() {
+        logger.info("Accessed /users endpoint");
+
+        return ResponseEntity.ok("Hello, User!");
+    }
+
 }
